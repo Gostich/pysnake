17cb5ed5ab986b4ee0e1c85e623841fb84d4c36c
Gosti <yannick.gosteli@petzi.ch>
2021-04-11 19:31:08 +0200

    move snake head

--- app/pysnake.py
+++ app/pysnake.py
@@ -1,10 +1,16 @@
 import pygame
 
 pygame.init()
-dis = pygame.display.set_mode((400, 300))
+dis = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('Snake game by Gosti (Edureka totorial)')
 
-blue = (0, 0, 255)
+white = (255, 255, 255)
+black = (0, 0, 0)
+
+x1 = 300
+y1 = 300
+
+clock = pygame.time.Clock()
 
 game_over = False
 while not game_over:
@@ -12,8 +18,29 @@ while not game_over:
         print(event)  # prints out all the actions that take place on the screen
         if event.type == pygame.QUIT:
             game_over = True
-        pygame.draw.rect(dis, blue, [200, 150, 10, 10])
+
+        x1_change = 0
+        y1_change = 0
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x1_change = -10
+                y1_change = 0
+            elif event.key == pygame.K_RIGHT:
+                x1_change = 10
+                y1_change = 0
+            elif event.key == pygame.K_UP:
+                y1_change = -10
+                x1_change = 0
+            elif event.key == pygame.K_DOWN:
+                y1_change = 10
+                x1_change = 0
+        x1 += x1_change
+        y1 += y1_change
+
+        dis.fill(white)
+        pygame.draw.rect(dis, black, [x1, y1, 10, 10])
         pygame.display.update()
 
-pygame.display.update()
+        clock.tick(30)
+
 pygame.quit()
